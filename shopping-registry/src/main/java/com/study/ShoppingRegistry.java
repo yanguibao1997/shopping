4b0be91a404fb7c611ce5103dfbd8d26e0c97c1e
yanguibao1997 <1713940247@qq.com>
2018-12-28 12:33:03 +0800

    项目后台搭建

---
+++ shopping-registry/src/main/java/com/study/ShoppingRegistry.java
@@ -0,0 +1,13 @@
+package com.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ShoppingRegistry {
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingRegistry.class,args);
+    }
+}
